1315b94e321c9163cd63a624a7e604157e6e48a5
Wai Man Chan <wmchanad@stu.ust.hk>
2015-06-24 12:00:30 +0800

    Pushing missing header

--- PHKAccessory.h
+++ PHKAccessory.h
@@ -92,20 +92,96 @@ typedef enum {
     charType_videoPropAttr      = 0x3B,
     charType_videoRotation      = 0x3C,
     charType_videoValAttr       = 0x3D,
+    
+#pragma - The following is only default by the device after iOS 9
+    
+    charType_firmwareRevision   = 0x52,
+    charType_hardwareRevision   = 0x53,
+    charType_softwareRevision   = 0x54,
+    
+    charType_reachable          = 0x63,
+    
+    charType_airParticulateDensity = 0x64,
+    charType_airParticulateSize = 0x65,
+    charType_carbonMonoxideDetected = 0x69,
+    charType_smokeDetected      = 0x76,
+    
+    charType_alarmCurrentState  = 0x66,
+    charType_alarmTargetState   = 0x67,
+    charType_batteryLevel       = 0x68,
+    charType_contactSensorState = 0x6A,
+    charType_holdPosition       = 0x6F,
+    charType_leakDetected       = 0x70,
+    
+    charType_currentAmbientLightLevel = 0x6B,
+    charType_currentHorizontalTiltAngle = 0x6C,
+    charType_targetHorizontalTiltAngle = 0x7B,
+    charType_currentPosition    = 0x6D,
+    charType_targetPosition     = 0x7C,
+    charType_currentVerticalTiltAngle = 0x6E,
+    charType_targetVerticalTiltAngle = 0x7D,
+    
+    charType_positionState      = 0x72,
+    charType_programmableSwitchEvent = 0x73,
+    charType_programmableSwitchOutputState = 0x74,
+    
+    charType_sensorActive       = 0x75,
+    charType_sensorFault        = 0x77,
+    charType_sensorJammed       = 0x78,
+    charType_sensorLowBattery   = 0x79,
+    charType_sensorTampered     = 0x7A,
+    
 
 #pragma - The following is service provide
-    charType_accessoryInfo      = 0x3E,
-    charType_camera             = 0x3F,
-    charType_fan                = 0x40,
-    charType_garageDoorOpener   = 0x41,
-    charType_lightBulb          = 0x43,
-    charType_lockManagement     = 0x44,
-    charType_lockMechanism      = 0x45,
-    charType_microphone         = 0x46,
-    charType_outlet             = 0x47,
-    charType_speaker            = 0x48,
-    charType_switch             = 0x49,
-    charType_thermostat         = 0x4A
+    serviceType_accessoryInfo      = 0x3E,
+    serviceType_camera             = 0x3F,
+    serviceType_fan                = 0x40,
+    serviceType_garageDoorOpener   = 0x41,
+    serviceType_lightBulb          = 0x43,
+    serviceType_lockManagement     = 0x44,
+    serviceType_lockMechanism      = 0x45,
+    serviceType_microphone         = 0x46,
+    serviceType_outlet             = 0x47,
+    serviceType_speaker            = 0x48,
+    serviceType_switch             = 0x49,
+    serviceType_thermostat         = 0x4A,
+    
+    serviceType_alarmSystem        = 0x7E,
+    serviceType_bridgingState      = 0x62,
+    serviceType_carbonMonoxideSensor = 0x7F,
+    serviceType_contactSensor      = 0x80,
+    serviceType_door               = 0x81,
+    serviceType_humiditySensor     = 0x82,
+    serviceType_leakSensor         = 0x83,
+    serviceType_lightSensor        = 0x84,
+    serviceType_motionSensor       = 0x85,
+    serviceType_occupancySensor    = 0x86,
+    serviceType_smokeSensor        = 0x87,
+    serviceType_programmableSwitch_stateful = 0x88,
+    serviceType_programmableSwitch_stateless = 0x89,
+    serviceType_temperatureSensor  = 0x8A,
+    serviceType_window             = 0x8B,
+    serviceType_windowCover        = 0x8C,
+    
+#pragma - The following is for bluetooth characteristic
+    btCharType_pairSetup = 0x4C,
+    btCharType_pairVerify = 0x4E,
+    btCharType_pairingFeature = 0x4F,
+    btCharType_pairings = 0x50,
+    btCharType_serviceInstanceID = 0x51,
+#pragma - The following is for bluetooth service
+    btServiceType_accessoryInformation = 0xFED3,
+    btServiceType_camera            = 0xFEC9,
+    btServiceType_fan               = 0xFECB,
+    btServiceType_garageDoorOpener  = 0xFECE,
+    btServiceType_lightBulb         = 0xFED2,
+    btServiceType_lockManagement    = 0xFECF,
+    btServiceType_lockMechanism     = 0xFED0,
+    btServiceType_microphone        = 0xFEC8,
+    btServiceType_outlet            = 0xFECC,
+    btServiceType_speaker           = 0xFEC7,
+    btServiceType_switch            = 0xFECD,
+    
 } charType;
 
 enum {
